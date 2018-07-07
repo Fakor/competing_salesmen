65751676e9bd93b07fa1b8cf62886b2ed7b43b50
Fakor <david.johansson35@gmail.com>
2018-07-07 23:08:46 +0200

    Started using argparser in command

--- src/cs_command_line.cpp
+++ src/cs_command_line.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <string>
+#include <random>
+#include <memory>
+
+#include "cxxopts.hpp"
 
 int main(int argc, char **argv){
-  std::string command_file;
-  if(argc==2){
-    command_file = argv[1];
+  cxxopts::Options options("Competing Salesman", "Command line version");
+  options.add_options()
+    ("s,seed", "Start seed number", cxxopts::value<int>())
+    ("r,rounds", "Number of rounds", cxxopts::value<int>()->default_value("1"));
+
+  auto result = options.parse(argc, argv);
+  int seed;
+  if(result.count("seed") == 0){
+    std::random_device rd;
+    seed=rd();
   } else{
-    return 1;
+    seed=result["seed"].as<int>();
   }
-  std::cout << "Command file: " << command_file << std::endl;
+  int rounds = result["rounds"].as<int>();
+
+  std::cout << "Seed: " << seed << std::endl;
+  std::cout << "Rounds: " << rounds << std::endl;
+
 
   return 0;
 }
